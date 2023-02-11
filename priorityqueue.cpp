4268e5a3637ef50ab105a55b2d20d56ca40478b6
Sophia Thompson <119273210+smthom1@users.noreply.github.com>
2023-02-10 16:25:15 -0800

    Update priorityqueue.cpp

--- priorityqueue.cpp
+++ priorityqueue.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include "json.hpp"
-
 #include "priorityqueue.h"
 
 PriorityQueue::PriorityQueue(std::size_t max_size) :
@@ -14,60 +13,110 @@ void PriorityQueue::insert(Key k) {
 }
 
 void PriorityQueue::insert(KeyValuePair kv) {
-	// TODO: complete this function
+	if (size_ >= max_size_) {
+		return;
+	}
+	nodes_[++size_] = kv;
+	heapifyUp(size_);
 }
 
 KeyValuePair PriorityQueue::min() {
-	// TODO: complete this function
+	if (size_ == 0) {
+		return KeyValuePair();
+	}
+	return nodes_[ROOT];
 }
 
 KeyValuePair PriorityQueue::removeMin() {
-	// TODO: complete this function
+	if (size_ == 0) {
+		return KeyValuePair();
+	}
+	KeyValuePair min = nodes_[ROOT];
+	nodes_[ROOT] = nodes_[size_--];
+	heapifyDown(ROOT);
+	return min;
 }
 
 bool PriorityQueue::isEmpty() const {
-	// TODO: complete this function
+	return size_ == 0;
 }
 
 size_t PriorityQueue::size() const {
-	// TODO: complete this function
+	return size_;
 }
 
 nlohmann::json PriorityQueue::JSON() const {
 	nlohmann::json result;
-  for (size_t i = 1; i <= size_; i++) {
-      nlohmann::json node;
-      KeyValuePair kv = nodes_[i];
-      node["key"] = kv.first;
-      node["value"] = kv.second;
-      if (i != ROOT) {
-          node["parent"] = std::to_string(i / 2);
-      }
-      if (2 * i <= size_) {
-          node["leftChild"] = std::to_string(2 * i);
-      }
-      if (2 * i + 1 <= size_) {
-          node["rightChild"] = std::to_string(2 * i + 1);
-      }
-      result[std::to_string(i)] = node;
-  }
-  result["metadata"]["max_size"] = max_size_;
-  result["metadata"]["size"] = size_;
+	for (size_t i = 1; i <= size_; i++) {
+		nlohmann::json node;
+		KeyValuePair kv = nodes_[i];
+		node["key"] = kv.first;
+		node["value"] = kv.second;
+		if (i != ROOT) {
+			node["parent"] = std::to_string(i / 2);
+		}
+		if (2 * i <= size_) {
+			node["leftChild"] = std::to_string(2 * i);
+		}
+		if (2 * i + 1 <= size_) {
+			node["rightChild"] = std::to_string(2 * i + 1);
+		}
+		result[std::to_string(i)] = node;
+	}
+	result["metadata"]["max_size"] = max_size_;
+	result["metadata"]["size"] = size_;
 	return result;
 }
 
 void PriorityQueue::heapifyUp(size_t i) {
-	// TODO: complete this function
+	if (i <= ROOT) {
+		return;
+	}
+	size_t parent = i / 2;
+	if (nodes_[i].first < nodes_[parent].first) {
+		std::swap(nodes_[i], nodes_[parent]);
+		heapifyUp(parent);
+	}
 }
 
 void PriorityQueue::heapifyDown(size_t i) {
-	// TODO: complete this function
+  size_t leftChild = 2 * i;
+  size_t rightChild = 2 * i + 1;
+  size_t smallest = i;
+  if (leftChild <= size_ && getKey(leftChild) < getKey(i)) {
+    smallest = leftChild;
+  }
+  if (rightChild <= size_ && getKey(rightChild) < getKey(smallest)) {
+    smallest = rightChild;
+  }
+  if (smallest != i) {
+    std::swap(nodes_[i], nodes_[smallest]);
+    heapifyDown(smallest);
+  }
 }
 
-void PriorityQueue::removeNode(size_t i) {
-	// TODO: complete this function
+KeyValuePair PriorityQueue::removeMin() {
+  KeyValuePair result = nodes_[ROOT];
+  nodes_[ROOT] = nodes_[size_];
+  size_--;
+  heapifyDown(ROOT);
+  return result;
+}
+
+bool PriorityQueue::isEmpty() const {
+  return size_ == 0;
+}
+
+size_t PriorityQueue::size() const {
+  return size_;
 }
 
 Key PriorityQueue::getKey(size_t i) {
-	// TODO: complete this function
+  return nodes_[i].first;
+}
+
+void PriorityQueue::removeNode(size_t i) {
+  nodes_[i] = nodes_[size_];
+  size_--;
+  heapifyDown(i);
 }
