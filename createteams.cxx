c48a1204839ba1b6b351580e46ca90fa0e84cea1
tenzifrenzi <115435821+tenzifrenzi@users.noreply.github.com>
2023-02-16 15:34:56 -0800

    Add files via upload

--- createteams.cxx
+++ createteams.cxx
@@ -2,96 +2,115 @@
 #include <fstream>
 #include <vector>
 #include "teamdata.h"
+#include "priorityqueue.h"
 
 using json = nlohmann::json;
 
-int main(int argc, char** argv){
-    if(argc != 2){
-        std::cerr << "Usage: " << argv[0] << " file.json" << std::endl;
-        return -1;
-    }
-    std::ifstream jsonFile(argv[1]);
-    if(!jsonFile){
-        std::cerr << "Error: Unable to open file" << std::endl;
-        return -1;
-    }
-    json teamData;
-    jsonFile >> teamData;
-    int totalTeams = (teamData["metadata"]["numPlayers"]) / 2;
-    int currentTeams = 0;
-    while(currentTeams < totalTeams) {
+bool playerInVector(int playerToFind, std::vector<int> teamVector);
 
-        for(int)
+int main(int argc, char** argv){
+    if(argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " file.json" << std::endl;
+        return 1;
     }
-    std::cout << teamData.dump(2) << std::endl;
+    // Read the JSON data
+    json data;
+    try {
+        std::ifstream jsonFile(argv[1]);
+        jsonFile >> data;
+    } catch (const std::exception& e) {
+        std::cerr << "Error reading input JSON: " << e.what() << std::endl;
+        return 1;
+    }
+
+    // Adds all possible 2-person team combinations to a priority queue
+    auto combinationArray = data["teamStats"];
+    PriorityQueue teamQueue(combinationArray.size());
+    for(int i = 0; i < (int)combinationArray.size(); i++) {
+        // Makes a pair with the winPercentage (key) and the current pair of teammembers (value) and adds information to the queue
+        teamQueue.insert(std::make_pair(std::abs((double)combinationArray[i]["winPercentage"] - 50.0),
+                                        std::make_pair((int)combinationArray[i]["playerOne"], (int)combinationArray[i]["playerTwo"])));
+    }
+
+    // Assign players to a team of vectors based on priority queue
+    std::vector<std::pair<int, int>> finalTeams;
+    //assigned vector contains all players already in a team
+    std::vector<int> assigned;
+
+    int numPlayers = data["metadata"]["numPlayers"];
+    //creates all teams using a greedy algorithm
+    while ((int)finalTeams.size() < numPlayers / 2) {
+        //gets the team still in the priority queue with the closest win percentage to 50
+        std::pair<int, int> team = teamQueue.min().second;
+        teamQueue.removeMin();
+        //Checks if both members aren't in a team yet
+        if (!playerInVector(team.first, assigned) && !playerInVector(team.second, assigned)) {
+            finalTeams.push_back(team);
+            assigned.push_back(team.first);
+            assigned.push_back(team.second);
+        }
+    }
+
+    // Output the resulting teams
+    nlohmann::json output;
+    output["teams"] = finalTeams;
+    std::cout << output.dump(2) << std::endl;
     return 0;
 }
 
-/* VERS. 2
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include "teamdata.h"
-
-using json = nlohmann::json;
-
-int main(int argc, char** argv) {
-  if (argc != 2) {
-    std::cerr << "Usage: " << argv[0] << " team_data.json\n";
-    return 1;
-  }
-
-  // Load team data
-  std::string filename(argv[1]);
-  TeamData teamData(filename);
-  int numPlayers = teamData.numPlayers();
-
-  // Priority queue to sort the 2-person teams by the difference from 50% win rate
-  std::priority_queue<std::pair<double, std::pair<int, int>>,
-                      std::vector<std::pair<double, std::pair<int, int>>>,
-                      std::greater<std::pair<double, std::pair<int, int>>>>
-      teamQueue;
-
-  // Add all 2-person teams to the priority queue
-  for (int i = 0; i < numPlayers; ++i) {
-    for (int j = i + 1; j < numPlayers; ++j) {
-      teamQueue.push(
-          std::make_pair(std::abs(teamData.winPercentages(i, j) - 50.0),
-                         std::make_pair(i, j)));
-    }
-  }
-
-  // Assign players to teams
-  std::vector<std::pair<int, int>> teams;
-  std::vector<bool> assigned(numPlayers, false);
-  while (teams.size() < numPlayers / 2) {
-    std::pair<int, int> team = teamQueue.top().second;
-    teamQueue.pop();
-
-    if (!assigned[team.first] && !assigned[team.second]) {
-      teams.push_back(team);
-      assigned[team.first] = true;
-      assigned[team.second] = true;
-
-      // Remove all teams with any player from this team
-      for (auto& q = teamQueue; !q.empty();) {
-        auto& t = q.top().second;
-        if (t.first == team.first || t.first == team.second ||
-            t.second == team.first || t.second == team.second) {
-          q.pop();
-        } else {
-          break;
+bool playerInVector(int playerToFind, std::vector<int> teamVector){
+    for(int player : teamVector){
+        if(playerToFind == player){
+            return true;
         }
-      }
     }
-  }
-
-  // Output the resulting teams
-  nlohmann::json output;
-  output["teams"] = teams;
-  std::cout << output.dump() << std::endl;
-
-  return 0;
+    return false;
 }
 
-*/
+/* Commented out this function as the program as is already doesn't look at such instances
+void removeAllInstancesWithPlayer(int player, PriorityQueue& pq){
+    //Search up: can you reference user made classes?
+    for(int i = 0; i < (int)pq.size(); i++){
+
+    }
+}/
+    // Load the data into the priority queue
+    const auto& teamStats = data["teamStats"];
+    for (const auto& stat : teamStats) {
+        const int playerOne = stat["playerOne"];
+        const int playerTwo = stat["playerTwo"];
+        const double winPercentage = stat["winPercentage"];
+        if (!usedPlayers[playerOne][playerTwo]) {
+            teamQueue.push(std::make_pair(winPercentage, std::make_pair(playerOne, playerTwo)));
+            usedPlayers[playerOne][playerTwo] = true;
+            usedPlayers[playerTwo][playerOne] = true;
+        }
+    }
+
+    // Create the teams
+    while (teams.size() < numPlayers / 2) {
+        const auto team = teamQueue.top();
+        teamQueue.pop();
+
+        const int playerOne = team.second.first;
+        const int playerTwo = team.second.second;
+        if (!usedPlayers[playerOne][playerTwo]) {
+            teams.push_back(team.second);
+            usedPlayers[playerOne][playerTwo] = true;
+            for (int i = 0; i < numPlayers; i++) {
+                usedPlayers[i][playerOne] = true;
+                usedPlayers[i][playerTwo] = true;
+            }
+        }
+    }
+
+    // Output the teams
+    json output;
+    output["teams"] = json::array();
+    for (const auto& team : teams) {
+        output["teams"].push_back({team.first, team.second});
+    }
+    std::cout << output << std::endl;
+    //std::cout << data.dump(2) << std::endl;
+    return 0;
+}*/
